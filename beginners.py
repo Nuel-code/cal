8066800a30245f193481cb15061e20bfee543bc1
Nuel-code <113852952+Nuel-code@users.noreply.github.com>
2023-03-02 06:54:15 -0700

    Update beginners.py

--- beginners.py
+++ beginners.py
@@ -6,7 +6,7 @@ else:
     print("GET OUT")
 country = input("country: ")
 if country == "nigeria" or "usa" or "ghana" or "colombia":
-    print("hi, ur name is gabrielle. i am from imo state, i love u")
+    print("hi, ur name is gabrielle. i am from ....., i love u")
 else:
     print("hi, my name is alisha. do u love me")
 
